8522f2ebe5a5c0a35e874be8673fe84b29add2c1
ArturVargas <arturvargas9226@gmail.com>
2019-08-06 20:50:03 -0500

    [create] arq ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { FormComponent } from './components/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'posts', component: PostsComponent},
+  {path:'post/:id', component: PostComponent},
+  {path:'newpost', component: FormComponent},
+  { path:'', pathMatch:'full',redirectTo:'/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
